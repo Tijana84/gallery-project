8b7a22c8311c296f17b65246f223f1be9aceba99
Tijana Savic <tijanasavic1984@gmail.com>
2022-01-19 21:31:14 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function Users(name,surname){
+  this.name = name;
+  this.surname = surname;
+  this.info = function(){
+      document.write(name+ ' ' +surname);
+  }
+}
+var tijana = new Users('Tijana', 'Savic');
+tijana.info();
+
+var current = document.getElementById("current");
+var pictures = document.getElementsByClassName("thumb");
+for (var i = 0; i < pictures.length; i++) {
+  pictures[i].addEventListener("click", display);
+}
+function display() {
+  var sl = this.getAttribute("src");
+  current.setAttribute("src", sl);
+}
